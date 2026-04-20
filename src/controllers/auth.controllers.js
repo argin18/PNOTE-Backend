53506b2479e4abd3f9b55aca4eccb7c57b3deb6f
sumitbhujel <arginbhujel1834@gmail.com>
2026-04-20 17:03:05 +0545

    update strict mode

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -15,7 +15,7 @@ const setTokenCookie = (res, token) =>
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
@@ -158,7 +158,7 @@ const logoutUser = (req, res) => {
     res.clearCookie("token", {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
     });
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
@@ -180,7 +180,7 @@ const getMe = async (req, res) => {
       res.clearCookie("token", {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
       })
       return res.status(403).json({ message: "Your account has been banned." })
     }
